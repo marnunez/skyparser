60456387c7f446b1fd126f7a10ae3b903318a503
marnunez <silver94@gmail.com>
2015-01-28 15:42:05 -0300

    Segundo commit

---
+++ navegador.rb
@@ -0,0 +1,27 @@
+class Navegador
+
+	require 'selenium-webdriver'
+	require 'nokogiri'
+
+	attr_accessor :driver, :ciudad1, :ciudad2, :fecha
+
+	def initialize(ciudad1 = nil ,ciudad2 = nil,fecha = nil)
+		@driver = Selenium::WebDriver.for :chrome
+		@ciudad1 = ciudad1
+		@ciudad2 = ciudad2
+		@fecha = fecha
+	end
+
+	def get_precio
+		@driver.navigate.to("http://www.skyscanner.net/transport/flights/#{@ciudad1}/#{@ciudad2}/#{@fecha}/)
+
+	wait = Selenium::WebDriver::Wait.new(:timeout => 15) # seconds
+	wait.until { driver.find_element(:class, "mainquote-price").displayed? }
+
+	return @driver.find_element(:class, "mainquote-price").text.gsub(/[\D]/,'')
+	end
+end
+
+
+
+	
